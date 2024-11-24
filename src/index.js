49f337cd5514ab814f171a6444c473c7582c037b
joan-15 <159147082+joan-15@users.noreply.github.com>
2024-11-24 18:12:17 -0500

    Se configuró una animacion para las imagenes de la galería

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.css'; // Ajusta la ruta según tu estructura
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
